a2c3c043b60bf6b437748bda73169e22df6e90cf
Md Adnan qaisar <staryounger012@gmail.com>
2025-12-05 09:55:46 +0530

    FullStack with Ai

---
+++ 03_datatypes/chapter_3.py
@@ -0,0 +1,41 @@
+# Integer
+
+black_tea_grams = 14
+ginger_grams = 3
+
+total_gram = black_tea_grams + ginger_grams
+print(f"total gram of baase tea is {total_gram}")
+
+reaming_tea = black_tea_grams - ginger_grams
+
+print(f"total gram of remaing tea is {reaming_tea}")
+
+milk_liters = 7
+servings = 4
+milk_per_serving = milk_liters / servings
+
+print(f"Milk per serving is {milk_per_serving}")
+
+total_tea_begs = 7
+pots = 4
+
+begs_per_pot = total_tea_begs // pots
+print(f"while tea begs per pot: {begs_per_pot}")
+
+
+total_cadamon_pods = 10
+pods_per_cup = 3
+leftover_pods = total_cadamon_pods / pods_per_cup
+
+print(f"Leftover C pods {leftover_pods}")
+
+base_flavor_strength = 2
+scale_factor = 3
+powerful_flavour = base_flavor_strength ** scale_factor
+
+print(f"Scaled flavoi=ur strength {powerful_flavour}")
+
+# 2 * 2 * 2
+
+total_tea_leaves_hervested = 1_000_000_000
+print(f"tea leaves : {total_tea_leaves_hervested}")
